cc6c509bba13186e1fcddc585147d162b89b0948
Levon Hovhannisyan <clevon85@outlook.com>
2021-01-29 18:32:28 +0400

    initial commit

---
+++ ProductsBlazorApp/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsBlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            string baseAddress = "http://localhost:7071/";
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
